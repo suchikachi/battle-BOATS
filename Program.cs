eb6440156d84b244c9ddd77e40e3dfdc269efcaf
deadlyfishes420 <richardhovsepyan12@gmail.com>
2022-12-31 19:30:39 +0000

    that didnt work

--- Program.cs
+++ Program.cs
@@ -10,28 +10,43 @@ using WW;
 namespace WW
 {
 #if WWLIB
-    public
+        public
 #endif
     static partial class StringUtility
     {
         public static string WordWrap(this string text, int width, params char[] wordBreakChars)
         {
-            if (string.IsNullOrEmpty(text) || width <= 0 || width >= text.Length) return text;
+            if (string.IsNullOrEmpty(text) || 0 == width || width >= text.Length) return text;
             var sb = new StringBuilder();
             var sr = new StringReader(text);
             string line;
+            var first = true;
             while (null != (line = sr.ReadLine()))
             {
+                var col = 0;
+                if (!first)
+                {
+                    sb.AppendLine();
+                    col = 0;
+                }
+                else first = false;
                 var words = line.Split(wordBreakChars);
+
                 for (var i = 0; i < words.Length; i++)
                 {
                     var word = words[i];
-                    if (i > 0) sb.Append(" ");
-                    if (sb.Length + word.Length > width)
+                    if (0 != i)
+                    {
+                        sb.Append(" ");
+                        ++col;
+                    }
+                    if (col + word.Length > width)
                     {
                         sb.AppendLine();
+                        col = 0;
                     }
                     sb.Append(word);
+                    col += word.Length;
                 }
             }
             return sb.ToString();
